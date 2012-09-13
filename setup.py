5721a6adebcd248128b14491e32ded81f68b5f7d
Kura <kura@deviling.net>
2012-09-13 16:35:03 +0100

    Updates for PyPI. Added Deiman requirement

--- setup.py
+++ setup.py
@@ -9,23 +9,22 @@ setup(name='blackhole',
       author="Kura",
       author_email="kura@kura.io",
       description="Tornado powered MTA for accepting all incoming emails without any disk I/O, although no messages actually ever get delivered. Mainly for testing huge send rates, for making sure developers don't accidentally send emails to real users, email integration testing and things like that.",
-      long_description=file(
-          os.path.join(
-              os.path.dirname(__file__),
-              'README.rst'
-          )
-      ).read(),
+      long_description=open("README.rst").read(),
       license='BSD',
       platforms=['linux'],
       packages=find_packages(exclude=["*.tests"]),
-      install_requires=file(os.path.join(os.path.dirname(__file__),
-                                         'requirements.txt')).read(),
+      install_requires=[
+          'tornado==2.3',
+          'setproctitle==1.1.6',
+          'deiman==0.1',
       scripts=[
           'blackhole/bin/blackhole',
       ],
       classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
+          'Development Status :: 4 - Beta',
+          'Operating System :: POSIX',
           'Operating System :: POSIX :: Linux',
+          'Operating System :: Unix',
           'Programming Language :: Python',
           'Topic :: Internet',
           'Topic :: Utilities',
