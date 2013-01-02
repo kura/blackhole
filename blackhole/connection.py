6d9105c050849fcbb22b45d5d1965bf3e2df879a
Kura <kura@deviling.net>
2013-01-02 04:41:41 -0800

    - Messed up the usage of setter 'cos I'm an idiot. This is now fixed and doesn't try to use the setter as a method... - Added a deleter to MailState, it shouldn't ever need to be used but it's there for the chance it does. - Added a del call on QUIT, this deletes the existing MailState object, it's not a requirement but could lead to memory not being freed.

--- blackhole/connection.py
+++ blackhole/connection.py
@@ -34,20 +34,22 @@ def connection_ready(sock, fd, events):
                 # Not exactly nice but it's only way I could safely figure
                 # out if it was the \n.\n
                 if line[0] == "." and len(line) == 3 and ord(line[0]) == 46:
-                    mail_state.reading(False)
+                    mail_state.reading = False
                     resp = response()
             elif any(line.lower().startswith(e) for e in ['helo', 'ehlo',
                                                           'mail from',
                                                           'rcpt to', 'rset']):
                 resp = response(250)
             elif line.lower().startswith("quit"):
+                # Destroy mail_state, just in-case
+                del mail_state
                 resp = response(221)
                 stream.write(resp)
                 stream.close()
                 return
             elif line.lower().startswith("data"):
                 resp = response(354)
-                mail_state.reading(True)
+                mail_state.reading = True
             else:
                 resp = response(500)
             if resp:
--- blackhole/state.py
+++ blackhole/state.py
@@ -3,8 +3,19 @@ class MailState():
 
     @property
     def reading(self):
+    	"""
+    	MailState's 'reading' property, used for
+    	figuring out where we are in the state chain.
+    	"""
         return self._reading_data
 
     @reading.setter
     def reading(self, val):
+    	"""Property for setting self.reading"""
         self._reading_data = val
+
+    @reading.deleter
+    def reading(self):
+    	"""Property for deleting self.reading"""
+    	# This shouldn't be called or need to be called
+    	del self._reading_data
