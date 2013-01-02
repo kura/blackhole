8933509f9102a662c2cd157ff7dae0f405c4eab5
Kura <kura@deviling.net>
2013-01-02 09:02:43 -0800

    Forgot to commit ssh_utils used for SSL sockets

---
+++ blackhole/ssl_utils.py
@@ -0,0 +1,28 @@
+import os
+import ssl
+
+from tornado.options import options
+
+
+sslkwargs = {
+    'do_handshake_on_connect': False,
+    'server_side': True,
+    'ssl_version': ssl.PROTOCOL_TLSv1,
+    'keyfile': options.ssl_key,
+    'certfile': options.ssl_cert,
+    'ca_certs': options.ssl_ca_certs_dir,
+    'ciphers': None,
+}
+
+
+class BlackholeSSLException(Exception):
+	pass
+
+
+def verify_ssl_opts():
+	if not options.ssl_key or not options.ssl_cert:
+		raise BlackholeSSLException("You need to set an SSL certificate and SSL key")
+	if not os.path.exists(options.ssl_cert):
+		raise BlackholeSSLException("Certificate '%s' does not exist" % options.ssl_cert)
+	if options.ssl_key and not os.path.exists(options.ssl_key):
+		raise BlackholeSSLException("Keyfile '%s' does not exist" % options.ssl_key)
