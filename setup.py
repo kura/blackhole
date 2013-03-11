f360af3de5cc4010bb1cc8f47277b1e95f5cd5fd
Kura <kura@deviling.net>
2013-03-11 17:54:46 +0000

    Added license read to setup.py. Commiting an old comment change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='blackhole',
       maintainer_email="kura@kura.io",
       description="Tornado powered MTA for accepting all incoming emails without any disk I/O, although no messages actually ever get delivered. Mainly for testing huge send rates, for making sure developers don't accidentally send emails to real users, email integration testing and things like that.",
       long_description=open("README.rst").read(),
-      license='BSD',
+      license=open("LICENSE").read(),
       platforms=['linux'],
       packages=find_packages(exclude=["*.tests"]),
       install_requires=[
