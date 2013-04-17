2edb7ea0b53f39bf781d9acafd066d1771b0db0d
Kura <kura@deviling.net>
2013-04-17 15:28:43 +0100

    Added socket test

---
+++ blackhole/tests/test_sockets.py
@@ -0,0 +1,30 @@
+import unittest
+import random
+import socket
+
+from tornado.options import options
+
+from blackhole.connection import sockets
+from blackhole.opts import *
+
+
+class TestSSLSocketIsSet(unittest.TestCase):
+
+    def setUp(self):
+        options.ssl = True
+        options.port = random.randint(5000, 10000)
+        options.ssl_port = random.randint(5000, 10000)
+
+    def test_ssl_socket_is_set(self):
+        self.assertIsInstance(sockets()['ssl'], socket.socket)
+
+
+class TestSSLSocketIsNotSet(unittest.TestCase):
+
+    def setUp(self):
+        options.port = random.randint(5000, 10000)
+        options.ssl_port = random.randint(5000, 10000)
+        options.ssl = False
+
+    def test_ssl_socket_is_set(self):
+        self.assertNotIn('ssl', sockets())
