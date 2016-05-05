34786f42c910beddfa543b42c61a219482d5f03e
Kura <kura@kura.io>
2016-05-05 22:34:08 +0100

    These tests all started failing and travisci thinks the tests always pass if it gets to test_smtp.py without any errors.

--- tests/test_smtp_switches.py
+++ tests/test_smtp_switches.py
@@ -35,7 +35,7 @@ class TestHeadersSwitchDisabled(unittest.TestCase):
     def test_headers_disabled(self):
         cfile = create_config(('dynamic_switch=false', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
         smtp.process_header('x-blackhole-mode: bounce')
         assert smtp.mode == 'accept'
@@ -43,7 +43,7 @@ class TestHeadersSwitchDisabled(unittest.TestCase):
     def test_headers_enabled(self):
         cfile = create_config(('dynamic_switch=true', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
         smtp.process_header('x-blackhole-mode: bounce')
         assert smtp.mode == 'bounce'
@@ -51,7 +51,7 @@ class TestHeadersSwitchDisabled(unittest.TestCase):
     def test_headers_default(self):
         cfile = create_config(('', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
         smtp.process_header('x-blackhole-mode: bounce')
         assert smtp.mode == 'bounce'
@@ -61,43 +61,43 @@ class TestHeadersSwitchDisabled(unittest.TestCase):
 class TestProcessHeaders(unittest.TestCase):
 
     def test_valid_mode_header(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
         smtp.process_header('x-blackhole-mode: bounce')
         assert smtp.mode == 'bounce'
 
     def test_invalid_mode_header(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
         smtp.process_header('x-blackhole-mode: help')
         assert smtp.mode == 'accept'
 
     def test_invalid_mode_header2(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
         smtp.process_header('x-some-mode: bounce')
         assert smtp.mode == 'accept'
 
     def test_valid_single_delay(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.delay is None
         smtp.process_header('x-blackhole-delay: 30')
         assert smtp.delay is 30
 
     def test_invalid_single_delay(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.delay is None
         smtp.process_header('x-blackhole-delay: abc')
         assert smtp.delay is None
 
     def test_valid_range_delay(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.delay is None
         smtp.process_header('x-blackhole-delay: 5, 10')
         assert smtp.delay in [5, 6, 7, 8, 9, 10]
 
     def test_invalid_range_delay(self):
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.delay is None
         smtp.process_header('x-blackhole-delay: abc, def')
         assert smtp.delay is None
@@ -109,27 +109,27 @@ class TestModeSwitch(unittest.TestCase):
     def test_mode_default(self):
         cfile = create_config(('', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'accept'
 
     def test_mode_invalid(self):
         cfile = create_config(('', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.mode = 'kura'
         assert smtp.mode == 'accept'
 
     def test_mode_valid(self):
         cfile = create_config(('', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.mode = 'bounce'
         assert smtp.mode == 'bounce'
 
     def test_mode_valid_overrides_config(self):
         cfile = create_config(('mode=bounce', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.mode == 'bounce'
         smtp.mode = 'accept'
         assert smtp.mode == 'accept'
@@ -141,19 +141,19 @@ class TestDelaySwitch(unittest.TestCase):
     def test_delay_not_enabled_or_set(self):
         cfile = create_config(('', ))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.delay is None
 
     def test_delay_from_config(self):
         cfile = create_config(('delay=30',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         assert smtp.delay is 30
 
     def test_delay_switch_overrides_config_single(self):
         cfile = create_config(('delay=30',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '60'
         assert smtp.delay is 60
         assert smtp.config.delay is 30
@@ -161,7 +161,7 @@ class TestDelaySwitch(unittest.TestCase):
     def test_delay_switch_range_overrides_config(self):
         cfile = create_config(('delay=30',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '40, 45'
         assert smtp.delay in [x for x in range(40, 46)]
         assert smtp.config.delay is 30
@@ -169,90 +169,90 @@ class TestDelaySwitch(unittest.TestCase):
     def test_delay_switch_invalid_single_value_no_config(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = 'fifteen'
         assert smtp.delay is None
 
     def test_delay_switch_invalid_single_value_config_60(self):
         cfile = create_config(('delay=30',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = 'fifteen'
         assert smtp.delay is 30
 
     def test_delay_switch_invalid_single_negative_value(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '-10'
         assert smtp.delay is None
 
     def test_delay_switch_not_above_max(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '90'
         assert smtp.delay is 60
 
     def test_delay_switch_invalid_range_value_no_config(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = 'fifteen, eighteen'
         assert smtp.delay is None
 
     def test_delay_switch_invalid_min_range_value_no_config(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = 'fifteen, 18'
         assert smtp.delay is None
 
     def test_delay_switch_invalid_max_range_value_no_config(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '15, eighteen'
         assert smtp.delay is None
 
     def test_delay_switch_invalid_range_negative_min_value(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '-10, 10'
         assert smtp.delay is None
 
     def test_delay_switch_invalid_range_negative_max_value(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '1, -10'
         assert smtp.delay is None
 
     def test_delay_switch_invalid_range_negatives(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '-10, -1'
         assert smtp.delay is None
 
     def test_delay_switch_range_min_higher_than_max(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '20, 10'
         assert smtp.delay is None
 
     def test_delay_switch_range_max_higher_than_60(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '59, 70'
         assert smtp.delay in [59, 60]
 
     def test_delay_switch_more_than_2(self):
         cfile = create_config(('',))
         Config(cfile).load()
-        smtp = Smtp()
+        smtp = Smtp(None, [])
         smtp.delay = '1, 2, 3'
         assert smtp.delay is None
