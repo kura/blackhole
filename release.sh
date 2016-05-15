84f49cba30764dc7d90c6d5462998f4162d07475
Kura <kura@kura.io>
2016-05-15 02:55:24 +0100

    Fixed release.sh

--- release.sh
+++ release.sh
@@ -5,11 +5,13 @@ echo "Current version: ${last_rel}"
 
 read -p "New version: " version
 
-sed -i "s/__version__.*/__version = '${version}'/g" blackhole/__init__.py
+sed -i "s/__version__.*/__version__ = '${version}'/g" blackhole/__init__.py
 git add blackhole/__init__.py
 git ci -m "${version}"
 git push origin master
 git tag $version
 git push --tags
-make release
+pip install wheel
+python setup.py sdist bdist_wheel upload
+rm -rf dist/build
 make web
