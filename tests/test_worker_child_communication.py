8d5228f4d2ca6df2d02935a87326e962f2efedbd
Kura <kura@kura.io>
2016-05-15 05:41:36 +0100

    Tests and fixes for mode and delay in listeners

--- tests/test_worker_child_communication.py
+++ tests/test_worker_child_communication.py
@@ -12,11 +12,13 @@ from ._utils import *
 @pytest.mark.usefixtures('reset_conf', 'reset_daemon', 'reset_supervisor',
                          'cleandir')
 @pytest.mark.asyncio
-async def test_ping_pong(event_loop):
+async def test_ping_pong():
     sock = _socket('127.0.0.1', 0, socket.AF_INET)
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)
     worker = Worker('1', [sock, ])
     await asyncio.sleep(30)
     worker.stop()
-    for task in asyncio.Task.all_tasks(event_loop):
+    for task in asyncio.Task.all_tasks(loop):
         task.cancel()
     assert worker._started is False
