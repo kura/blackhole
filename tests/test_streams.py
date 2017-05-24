ebd6f867269b7f84bb27edef18faba2ccbb627fb
Kura <kura@kura.io>
2017-05-24 01:47:47 +0100

    Adding tests to code that was previously missing tests.

---
+++ tests/test_streams.py
@@ -0,0 +1,39 @@
+import asyncio
+import os
+import socket
+from unittest import mock
+
+import pytest
+
+from blackhole.streams import StreamProtocol
+
+from ._utils import (cleandir, reset_conf, reset_daemon, reset_supervisor,
+                     create_config, create_file, Args)
+
+
+@pytest.mark.usefixtures('reset_conf', 'reset_daemon', 'reset_supervisor',
+                         'cleandir')
+@pytest.mark.asyncio
+async def test_client_not_connected():
+    sp = StreamProtocol()
+    assert sp.is_connected() is False
+
+
+@pytest.mark.usefixtures('reset_conf', 'reset_daemon', 'reset_supervisor',
+                         'cleandir')
+@pytest.mark.asyncio
+async def test_client_connected(event_loop):
+    sp = StreamProtocol(loop=event_loop)
+    sp.connection_made(asyncio.Transport())
+    assert sp.is_connected() is True
+
+
+# @pytest.mark.usefixtures('reset_conf', 'reset_daemon', 'reset_supervisor',
+#         'cleandir')
+# @pytest.mark.asyncio
+# async def test_connection_lost_exception(event_loop):
+#     sp = StreamProtocol(loop=event_loop)
+#     sp.connection_made(asyncio.Transport())
+#     assert sp.is_connected() is True
+#     with pytest.raises(KeyError):
+#         sp.connection_lost(KeyError)
