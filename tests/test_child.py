c1f43aa2bdd57d9c2552a3d23ed33ab776cc73f9
Kura <kura@kura.io>
2017-06-16 12:35:15 +0100

    Merged in 'lint-radon' to enable radon checks

--- tests/test_child.py
+++ tests/test_child.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# pylama:skip=1
+
 import asyncio
 import os
 import socket
--- tests/test_logs.py
+++ tests/test_logs.py
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# pylama:skip=1
+
 import logging
 
 import pytest
