5e6fe298596edfa5ed1324c1dbf72106362681f2
Kura <kura@kura.io>
2017-08-12 19:58:35 +0100

    version bump release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env sh
-
-last_rel=`blackhole -v`
-echo "Current version: ${last_rel}"
-
-printf "New version: "
-read version
-
-sed -i "s/__version__.*/__version__ = '${version}'/g" blackhole/__init__.py
-git add blackhole/__init__.py
-git ci -m "${version}"
-git push origin master
-git tag $version
-git push --tags
-pip install wheel
-python setup.py sdist bdist_wheel upload
-rm -rf dist/build
+pip install wheel twine
+python setup.py sdist bdist_wheel
+twine upload dist/*.whl dist/*.tar.*
