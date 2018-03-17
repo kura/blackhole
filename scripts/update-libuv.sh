ca7ccc4a967d647d425e06c90772d4ada910fa56
Kura <kura@kura.io>
2018-03-17 21:18:19 +0000

    Update Travis

--- scripts/update-libuv.sh
+++ scripts/update-libuv.sh
@@ -2,10 +2,10 @@
 wget https://api.github.com/repos/libuv/libuv/tarball -O libuv.tar.gz
 mkdir libuv
 tar xzvf libuv.tar.gz -C libuv --strip-components 1
-cd libuv
+pushd libuv
 ./autogen.sh
 ./configure
 make
 sudo make install
-cd ..
+popd
 rm -rf libuv*
