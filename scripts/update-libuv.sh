d9d1a1708a84c8689126c264818049c8deb1b460
Kura <kura@kura.io>
2018-03-19 22:52:22 +0000

    Fix Travis

--- scripts/update-libuv.sh
+++ scripts/update-libuv.sh
@@ -2,10 +2,10 @@
 wget https://api.github.com/repos/libuv/libuv/tarball -O libuv.tar.gz
 mkdir libuv
 tar xzvf libuv.tar.gz -C libuv --strip-components 1
-cd libuv
+cd libuv || return
 ./autogen.sh
 ./configure
 make
 sudo make install
-cd ..
+cd .. || return
 rm -rf libuv*
